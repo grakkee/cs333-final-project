d5e883f572b722052b02afb2add301556d94e314
Grace Meredith <gmeredith@nevada.unr.edu>
2023-05-02 11:46:11 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Grace Meredith
 CS333 Final Project
+2 May 2023
+
+This project was my hw1 assignment in Fall 2022 Design Patterns re-written in python.
+
+To view coverage report, go to Actions -> latest job run on feature branch -> Display Coverage Report
--- tests.py
+++ tests.py
@@ -1,8 +1,53 @@
 import unittest
+
+#order classes
 from order_computer import OrderComputer
+from order_ticket import OrderTicket
+from order_behavior import OrderBehaviorInterface
+
+#payment classes
+from payment_card import PaymentCard
+from payment_cash import PaymentCash
+from payment_behavior import PaymentBehaviorInterface
+
+#shaved ice classes
+from shaved_ice_shop import ShavedIceShop
+from kiosk import Kiosk
+from store import Store
 
 class TestUnits(unittest.TestCase):
+
+    #
+    #
+    #unit tests
+    #
+    #
+
     def test_order(self):
         o = OrderComputer()
 
         self.assertTrue(o.order())
+    
+
+    #
+    #
+    # integration tests
+    #
+    #
+
+    def test_order_behavior_with_computer(self):
+        #
+    
+    def test_order_behavior_with_ticket(self):
+        #
+    
+    def test_payment_behavior_with_cash(self):
+        #
+    
+    def test_payment_behavior_with_card(self):
+        #
+    
+    def test_shaved_ice_shop_with_kiosk(self):
+        #
+    
+    def test_shaved_ice_shop_with_store(self):
